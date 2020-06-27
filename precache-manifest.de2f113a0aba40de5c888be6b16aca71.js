c738d582a140983702544a58e7d5f9c11cd37883
Victor Batigin <bixs85@gmail.com>
2020-06-27 16:57:29 +0300

    Updates

---
+++ precache-manifest.de2f113a0aba40de5c888be6b16aca71.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c864d12f2b039c566c776107f9bde137",
+    "url": "/goit-react-hw-03-image-finder/index.html"
+  },
+  {
+    "revision": "11e079e45c481a5705ca",
+    "url": "/goit-react-hw-03-image-finder/static/css/2.f87e9f44.chunk.css"
+  },
+  {
+    "revision": "aba69d77d6931b9ecccb",
+    "url": "/goit-react-hw-03-image-finder/static/css/main.3947cc30.chunk.css"
+  },
+  {
+    "revision": "11e079e45c481a5705ca",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.4403f4fa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.4403f4fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aba69d77d6931b9ecccb",
+    "url": "/goit-react-hw-03-image-finder/static/js/main.1d9293b1.chunk.js"
+  },
+  {
+    "revision": "00ed64b4697a879a9e95",
+    "url": "/goit-react-hw-03-image-finder/static/js/runtime-main.4e03020b.js"
+  }
+]);
